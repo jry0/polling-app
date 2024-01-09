58ad640f60629f69917e3c03f30054cadd4f234e
Jerry Xing <j27xing@uwaterloo.ca>
2024-01-09 16:16:47 -0500

    test: added tests for was_pushblished_within_day()

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_was_published_within_day_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_within_day(), False)
+
+    def test_was_published_within_day_older_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_within_day(), False)
+
+    def test_was_published_within_day_recent_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(hours= 23, minutes=59, seconds=59)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_within_day(), True)
